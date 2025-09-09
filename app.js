3014012ca287658d731458466da46b620a3be0c0
HaseebHarry <haseebharry07@gmail.com>
2025-09-10 01:43:58 +0500

    Initial commitDD

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const express = require('express');
+// app.js
 const mongoose = require('mongoose');
 
-const app = express();
-const PORT = 3000;
+const MONGO_URI = 'mongodb+srv://haseebharry07_db_user:T7QYyyI0OVVPz5Dw@cluster0.mongodb.net/themeBuilderDB?retryWrites=true&w=majority';
 
-// Middleware to parse JSON
-app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydb', {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error:', err));
-
-// Simple route
-app.get('/', (req, res) => {
-    res.send('Hello World from Node.js!');
-});
-
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
