74f9d5bf922a4c15dc565a460d2df73c13a48829
HaseebHarry <haseebharry07@gmail.com>
2025-09-22 22:24:07 +0500

    asdf

--- models/UserTheme.js
+++ models/UserTheme.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const themeSchema = new mongoose.Schema({
   rlNo: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  agencyId: { type: String, required: true, unique: true },
+  agencyId: { type: String, required: true, unique: false },
   themeData: { type: Object, default: {} },  // stores all CSS variables dynamically
   selectedTheme: String,
   bodyFont: String,
