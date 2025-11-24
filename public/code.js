332980cc28c5d33979d16c51ccfab708bde86fdd
HaseebHarry <haseebharry07@gmail.com>
2025-11-24 22:38:15 +0500

    updated code

--- public/code.js
+++ public/code.js
@@ -853,5 +853,9 @@ function forceSidebarOpen() {
     if (themeName === "BlueWave Theme" && isSubAccount) {
         window.__BLUEWAVE_TOPNAV_ENABLED__ = true;
         enableBlueWaveTopNav();
-    }
+    }else {
+         window.__BLUEWAVE_TOPNAV_ENABLED__ = false;
+         resetGhlSidebar();
+         disableBlueWaveTopNav();
+     }
 })();
